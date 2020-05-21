b011178b9f22f75b290e2246e951a2baa86aa90a
ffelipercamargo <58056959+ffelipercamargo@users.noreply.github.com>
2020-05-20 21:51:03 -0300

    Create index.js

---
+++ Atividade6/index.js
@@ -0,0 +1,14 @@
+var Aluno1 = {
+  ra: '0030481813029',
+  nome: 'Felipe'
+}
+console.log(Aluno1);
+
+Aluno1['ra'] = '0030481813029';
+Aluno1['nome'] = 'Felipe3';
+console.log(Aluno1);
+
+Aluno1.ra = '0030481813029';
+Aluno1.nome = 'Felipe2';
+console.log(Aluno1);
+
